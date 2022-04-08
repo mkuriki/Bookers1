92ca7a956a8d2828a52b54e6aeaaf62d4df91cdc
mkuriki <101915061+mkuriki@users.noreply.github.com>
2022-04-08 10:46:11 +0000

    [Finish]bookのupdate機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
 
   get '/top' => "homes#top"
   post '/books/new' => "books#create"
+  patch 'books/:id' => 'books#update', as: 'update_book'
   
   resources :books,:homes
 
-  
+  root to:'homes#top'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
