e32eb4dc89d87841b860122a706f5c9daa6cc574
mkuriki <101915061+mkuriki@users.noreply.github.com>
2022-04-11 01:46:08 +0000

    [Finish]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,54 +1,54 @@
 class BooksController < ApplicationController
 
-  def create
-   @book = Book.new(book_params)
-   if @book.save
-      redirect_to book_path(@book.id)
-   else
-      @books = Book.all
-      render :index
-   end
-   
-   if @book.save
-     flash[:notice] = "Book was successfully created."
-   end
-  end
-  
-  def index
-    @book = Book.new
-    @books = Book.all
-  end
-  
-  def show
-    @book = Book.find(params[:id])
-  end
-  
-  def edit
-    @book = Book.find(params[:id])
-  end
-  
-  def update
-    @book = Book.find(params[:id])
-    @book.update(book_params)
-    
-   if @book.save
-     redirect_to book_path(@book.id)
-     flash[:notice] = "Book was successfully updated."
-   else
-     render :edit
-   end
-   
-  end
-  
-  def destroy
-    book = Book.find(params[:id])
-    book.destroy
-    redirect_to '/books'
-  end
-  
-  private
-  # ストロングパラメータ
-  def book_params
-    params.require(:book).permit(:title, :body)
-  end
+        def create
+           @book = Book.new(book_params)
+           
+           if @book.save
+              redirect_to book_path(@book.id)
+           else
+              @books = Book.all
+              render :index
+           end
+           
+           if @book.save
+             flash[:notice] = "Book was successfully created."
+           end
+        end
+      
+        def index
+            @book = Book.new
+            @books = Book.all
+        end
+      
+        def show
+          @book = Book.find(params[:id])
+        end
+      
+        def edit
+         @book = Book.find(params[:id])
+        end
+      
+        def update
+            @book = Book.find(params[:id])
+            @book.update(book_params)
+            
+           if @book.save
+             redirect_to book_path(@book.id)
+             flash[:notice] = "Book was successfully updated."
+           else
+             render :edit
+           end
+        end
+      
+        def destroy
+            book = Book.find(params[:id])
+            book.destroy
+            redirect_to '/books'
+        end
+      
+        private
+        # ストロングパラメータ
+        def book_params
+            params.require(:book).permit(:title, :body)
+        end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2022_04_10_064657) do
-
-  create_table "books", force: :cascade do |t|
-    t.string "title"
-    t.string "body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
+    create_table "books", force: :cascade do |t|
+      t.string "title"
+      t.string "body"
+      t.datetime "created_at", precision: 6, null: false
+      t.datetime "updated_at", precision: 6, null: false
+    end
 end
